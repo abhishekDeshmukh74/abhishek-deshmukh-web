62a62d165477ae4e47b0445b84945191886c660a
Abhishek Deshmukh <abhishekdeshmukh74@gmail.com>
2023-04-13 22:16:51 +0530

    assets

---
+++ src/environments/constants.ts
@@ -0,0 +1,43 @@
+export const CONSTANTS = {
+    BLOG_TOPICS: {
+        EVERYTHING: 'Everything',
+        ANGULAR: 'Angular',
+        JAVASCRIPT_ES6: 'JavaScript-ES6+',
+        REACT: 'React',
+        HTML_CSS: 'HTML & CSS',
+    },
+    SENTRY: {
+        DSN: 'https://f8a05810007749319c8b9638d4edd1fe@sentry.io/1895670'
+    },
+    LOCAL_STORAGE_KEYS: {
+        TOKEN: 'token',
+        USER: 'user',
+        IS_LOGGED_IN: 'isLoggedIn',
+    },
+    USERNAMES: {
+        TWITTER: '4bhi__deshmukh',
+        INSTAGRAM: 'abhishek__deshmukh',
+        GITHUB: 'abhishekDeshmukh74',
+        HACKERRANK: 'M0rpheu5',
+        YOUTUBE: 'UCzZeBOATg2XknvLwTFzNJDg',
+        LINKEDIN: 'abhishek-deshmukh-dev',
+        FACEBOOK: 'abhishekdeshmukh74'
+    },
+    REGEX: {
+        EXTENSION_FROM_FILENAME: /[^\.]+$/,
+        USERNAME_FROM_TEXT: /^@[a-zA-Z]+$/,
+        FULLNAME_FROM_TEXT: /^@[A-Za-z]+\s[A-Za-z]+/,
+    },
+    BLOG_POST_DISPLAY_SIZE: 4,
+    COMMENTS_DISPLAY_SIZE: 10,
+    ADSENSE_ID: 'ca-pub-1396362896371338',
+    ALERT_MESSAGES: {
+        BROWSER_NOT_SUPPORTED: 'Browser not supported',
+        LOGOUT_SUCCESS: 'Log out success!',
+        SUBSCRIBE_TO_PUSH_NOTIFICATIONS: 'Subscribed to Push Notifications',
+        COMMENT_VALIDATION_FAILED: 'Fix comment validations',
+        COMMENT_ADDED: 'Comment posted',
+        NOTIFICATION_SENT: 'Notification sent',
+        ADMIN_RESTRICTED_AREA: 'Admin restricted area'
+    }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:3000/api',
+  // apiUrl: 'https://abhishek-deshmukh-api.herokuapp.com/api',
+  vapidPublicKey: 'BG2XarIqKVn-BROxIuK29nyC-mp94eLy1_Woh7eLJIyK--ThvanU4hkF4CCD7Bw3EEQeFeuAxhs_2FfIeNEFl6s',
+  CONSENT_URL: 'https://accounts.google.com/o/oauth2/v2/auth?access_type=offline&scope=openid%20profile%20email&response_type=code&client_id=361894516073-ajj8ibs8q58aaiu60gf7ukdki4maensh.apps.googleusercontent.com&redirect_uri=http%3A%2F%2Flocalhost%3A4200%2Fblog%2Fgoogle%2Fcallback',
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
