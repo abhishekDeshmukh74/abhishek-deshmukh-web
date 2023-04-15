f7d527534f6f8c2b7a35dee251f2e31ffae9a8d9
Abhishek Deshmukh <abhishekdeshmukh74@gmail.com>
2023-04-15 09:02:42 +0530

    scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -81,7 +81,7 @@ export class AppComponent implements OnInit, AfterViewInit, OnDestroy {
   }
 
   getDepth(outlet: RouterOutlet): number {
-    return outlet.activatedRouteData.depth;
+    return outlet.activatedRouteData['depth'];
   }
 
   ngOnDestroy() {
