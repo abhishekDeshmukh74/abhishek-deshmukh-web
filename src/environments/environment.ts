a3a042b2c657febc5bc918117ce37a3c33b5b8db
Abhishek Deshmukh <abhishekdeshmukh74>
2024-06-23 21:00:14 +0530

    api url change

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   // apiUrl: 'http://localhost:3000/api',
-  apiUrl: 'https://abhishek-deshmukh-api.herokuapp.com/api',
+  apiUrl: 'https://abhishek-deshmukh-api.vercel.app.com/api',
   vapidPublicKey: 'BG2XarIqKVn-BROxIuK29nyC-mp94eLy1_Woh7eLJIyK--ThvanU4hkF4CCD7Bw3EEQeFeuAxhs_2FfIeNEFl6s',
   CONSENT_URL: 'https://accounts.google.com/o/oauth2/v2/auth?access_type=offline&scope=openid%20profile%20email&response_type=code&client_id=361894516073-ajj8ibs8q58aaiu60gf7ukdki4maensh.apps.googleusercontent.com&redirect_uri=http%3A%2F%2Flocalhost%3A4200%2Fblog%2Fgoogle%2Fcallback',
 };
