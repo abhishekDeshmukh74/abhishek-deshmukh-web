b0c90a88c88aef793ffa2126805a92670c8903f7
Abhishek Deshnukh <abhishekdeshmukh74@gmail.com>
2024-11-07 14:02:17 +0530

    backend update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,8 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://abhishek-deshmukh-api.vercel.app.com/api',
-  vapidPublicKey: 'BG2XarIqKVn-BROxIuK29nyC-mp94eLy1_Woh7eLJIyK--ThvanU4hkF4CCD7Bw3EEQeFeuAxhs_2FfIeNEFl6s',
-  CONSENT_URL: 'https://accounts.google.com/o/oauth2/v2/auth?access_type=offline&scope=openid%20profile%20email&response_type=code&client_id=361894516073-ajj8ibs8q58aaiu60gf7ukdki4maensh.apps.googleusercontent.com&redirect_uri=https%3A%2F%2Fwww.abhishekdeshmukh.com%2Fblog%2Fgoogle%2Fcallback',
+  apiUrl: 'https://abhishek-deshmukh-api.onrender.com/api',
+  vapidPublicKey:
+    'BG2XarIqKVn-BROxIuK29nyC-mp94eLy1_Woh7eLJIyK--ThvanU4hkF4CCD7Bw3EEQeFeuAxhs_2FfIeNEFl6s',
+  CONSENT_URL:
+    'https://accounts.google.com/o/oauth2/v2/auth?access_type=offline&scope=openid%20profile%20email&response_type=code&client_id=361894516073-ajj8ibs8q58aaiu60gf7ukdki4maensh.apps.googleusercontent.com&redirect_uri=https%3A%2F%2Fwww.abhishekdeshmukh.com%2Fblog%2Fgoogle%2Fcallback',
 };
